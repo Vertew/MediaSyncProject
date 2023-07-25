349e8589bcd3b25b60ea38c266249343f3d047f9
Sam Tudberry <sam@tudberry.net>
2023-07-25 17:06:06 +0100

    Fixed issue when there were no users in room

--- resources/views/livewire/friend-rooms.blade.php
+++ resources/views/livewire/friend-rooms.blade.php
@@ -5,15 +5,17 @@
     <div class="container-sm mt-3">
         <div class="list-group">
             <a class="list-group-item list-group-item-action" href = "{{route('rooms.show', ['key'=> $room->key])}}">{{$room->name}} - Owner: {{$room->user->username}}</a>
-            @foreach ($user_array[$room->id]?->users as $user)
-                {{-- This if statement is a bit of a cheaty way to get around the fact that the websocket channel doesn't update
-                     all clients quick enough to avoid displaying your name in the list of online users if you just left the room.
-                     You could refresh the list periodically but atm it doesn't seem worth the effort --}}
-                <p hidden>{{$username = App\Models\User::find($user->id)->username}}</p>
-                @if($username != Auth::user()->username)
-                    <p>{{$username}}</p>
-                @endif
-            @endforeach
+            @if(isset($user_array[$room->id]))
+                @foreach ($user_array[$room->id]->users as $user)
+                    {{-- This if statement is a bit of a cheaty way to get around the fact that the websocket channel doesn't update
+                        all clients quick enough to avoid displaying your name in the list of online users if you just left the room.
+                        You could refresh the list periodically but atm it doesn't seem worth the effort --}}
+                    <p hidden>{{$username = App\Models\User::find($user->id)->username}}</p>
+                    @if($username != Auth::user()->username)
+                        <p>{{$username}}</p>
+                    @endif
+                @endforeach
+            @endif
         </div>
     </div>
 @empty
