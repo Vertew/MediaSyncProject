721e659d59b18819cfb6df84dcb84ff6db131267
Sam Tudberry <sam@tudberry.net>
2023-09-07 15:41:21 +0100

    Minor adjustment to presence channel definition

--- routes/channels.php
+++ routes/channels.php
@@ -18,7 +18,7 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('presence.chat.{room_id}', function(User $user, int $id){
+Broadcast::channel('presence.chat.{room_id}', function(User $user){
     return $user;
 });
 
